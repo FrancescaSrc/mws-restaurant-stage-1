41dee3432183bef330765634c23d5de3e6c61a65
Francesca Paulin <fpaulin@solvedo.com>
2018-03-22 21:22:34 +0100

    FEAT: Add responsiveness to images
    Add srcset and sizes for different viewports in javascrip
    Create and compress images with grunt.js
    Test on different devices and make some changes in css accordingly

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+         engine: 'im',
+          sizes: [{
+            width: 200,
+            name: "small",
+            quality: 70
+          },{
+            width: 350,
+			name: "medium",
+            quality: 70
+          },
+		  {
+            width: 500,
+            name: "large",
+            quality: 70
+          },
+		  
+		  {
+            width: 600,
+            name: "large6",
+            quality: 80
+          },
+		  {
+            width: 800,
+            name: "large",
+            suffix: "_x2",  // retina gfx
+            quality: 90
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,webp}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
